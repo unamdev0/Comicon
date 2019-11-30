13d312dbe44e839cc42a597e0682c62da993f860
Udit Namdev <unamdev0@gmail.com>
2019-11-30 19:21:59 +0530

    Initial Commit

---
+++ src/components/headerFooter/header.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import MenuIcon from "@material-ui/core/Menu";
+import IconButton from "@material-ui/core/IconButton";
+
+export default class Header extends Component {
+  render() {
+    return (
+      <AppBar position="fixed" style={{ backgroundColor: "black" }}>Header</AppBar>
+    );
+  }
+}
