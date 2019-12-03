82aa998058259138afe5a0ab8d2698221c885c13
Udit Namdev <unamdev0@gmail.com>
2019-12-03 23:08:33 +0530

    Added scrolling feature through navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import React from 'react';
-import Carousel from './components/carousel/index'
-import Header from './components/headerFooter/header'
-import Info from './components/info/index'
-import Hightlight from './components/highlights/index'
-import Pricing from './components/pricing';
-import Location from './components/location/index'
-import Footer from './components/headerFooter/footer'
-import './resources/styles.css'
+import React from "react";
+import { Element } from "react-scroll";
+import Carousel from "./components/carousel/index";
+import Header from "./components/headerFooter/header";
+import Info from "./components/info/index";
+import Hightlight from "./components/highlights/index";
+import Pricing from "./components/pricing";
+import Location from "./components/location/index";
+import Footer from "./components/headerFooter/footer";
+import "./resources/styles.css";
 
 function App() {
   return (
-    <div className="App" style={{height:'1500px'}}>
-     <Header/>
-     <Carousel/>
-     <Info/>
-     <Hightlight/>
-     <Pricing/>
-     <Location/>
-     <Footer/>
+    <div className="App" style={{ height: "1500px" }}>
+      <Element name="header">
+        <Header />
+      </Element>
+      <Element name="carousel">
+        <Carousel />
+      </Element>
+      <Element name="info">
+        <Info />
+      </Element>
+      <Element name="highlight">
+        <Hightlight />
+      </Element>
+      <Element name="pricing">
+        <Pricing />
+      </Element>
+      <Element name="location">
+        <Location />
+      </Element>
+      <Element name="footer">
+        <Footer />
+      </Element>
     </div>
   );
 }
--- src/components/headerFooter/sideDrawer.js
+++ src/components/headerFooter/sideDrawer.js
@@ -1,29 +1,69 @@
 import List from "@material-ui/core/List";
+import { scroller } from "react-scroll";
 import ListItem from "@material-ui/core/ListItem";
 import Drawer from "@material-ui/core/Drawer";
 import React from "react";
 
 const SideDrawer = props => {
+  const scrollToElement = element => {
+    scroller.scrollTo(element, {
+      delay: 100,
+      duration: 1500,
+      smooth: true
+    });
+  };
   return (
-   <Drawer
-   anchor="right"
-   open={props.open}
-   onClose={()=>{props.close(false)}}>
-       <List component="nav">
-           <ListItem button onClick={()=>{}}>
-               Events start in 
-           </ListItem>
-           <ListItem button onClick={()=>{}}>
-               Price            
-           </ListItem>
-           <ListItem button onClick={()=>{}}>
-               Featuring
-           </ListItem>
-           <ListItem button onClick={()=>{}}>
-             More Info
-           </ListItem>
-       </List>
-   </Drawer>
+    <Drawer
+      anchor="right"
+      open={props.open}
+      onClose={() => {
+        props.close(false);
+      }}
+    >
+      <List component="nav">
+        <ListItem
+          button
+          onClick={() => {
+            scrollToElement("carousel");
+          }}
+        >
+          Event starts in
+        </ListItem>
+        <ListItem
+          button
+          onClick={() => {
+            scrollToElement("info");
+          }}
+        >
+          Info
+        </ListItem>
+
+        <ListItem
+          button
+          onClick={() => {
+            scrollToElement("highlight");
+          }}
+        >
+          highlight{" "}
+        </ListItem>
+        <ListItem
+          button
+          onClick={() => {
+            scrollToElement("pricing");
+          }}
+        >
+          Price
+        </ListItem>
+        <ListItem
+          button
+          onClick={() => {
+            scrollToElement("location");
+          }}
+        >
+          Location
+        </ListItem>
+      </List>
+    </Drawer>
   );
 };
 
